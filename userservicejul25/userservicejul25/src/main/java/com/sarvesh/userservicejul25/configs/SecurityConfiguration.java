7eb725eb6a66ad3b7a7a9e36c3c3a94ae6baf512
Sarvesh Dutt Richhariya <sarveshdutt40@gmail.com>
2025-08-18 12:53:26 +0530

    implementing OAuth2

--- userservicejul25/userservicejul25/src/main/java/com/sarvesh/userservicejul25/configs/SecurityConfiguration.java
+++ userservicejul25/userservicejul25/src/main/java/com/sarvesh/userservicejul25/configs/SecurityConfiguration.java
@@ -7,21 +7,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-        @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> {
-                            try {
-                                requests
-                                        .anyRequest().permitAll()
-                                        .and().cors().disable()
-                                        .csrf().disable();
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                );
-
-        return http.build();
-    }
+//        @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> {
+//                            try {
+//                                requests
+//                                        .anyRequest().permitAll()
+//                                        .and().cors().disable()
+//                                        .csrf().disable();
+//                            } catch (Exception e) {
+//                                throw new RuntimeException(e);
+//                            }
+//                        }
+//                );
+//
+//        return http.build();
+//    }
 }
